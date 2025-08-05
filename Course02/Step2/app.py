1c183c43f63778c5f1a217b5cb51fe84d910f54c
ted111712 <ted111712@gmail.com>
2025-08-05 15:50:37 +0900

    Feat: 메뉴 화면추가
    추가한 기능 설명
    
    Resolves: #1

--- Course02/Step2/app.py
+++ Course02/Step2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world() -> str:
     return 'Hello World!'
+
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
 
 
 if __name__ == '__main__':

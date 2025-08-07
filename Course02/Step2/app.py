1ff7d3863ce87d424d4890bb701409be08aa7a53
ted111712 <ted111712@gmail.com>
2025-08-07 18:32:26 +0900

    Add(2-2-4): 순순 app.py 추가

---
+++ Course02/Step2/app.py
@@ -0,0 +1,30 @@
+import socket
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    assert 1 < 0, 'error'
+    if app.debug:
+        hostname = '컴퓨터(인스턴스):' + socket.gethostname()
+    else:
+        hostname = ''
+    return render_template('index.html', computername=hostname)
+
+
+@app.route('/test1')
+def test1():
+    return render_template('test1.html')
+
+
+@app.route('/test2')
+def test2():
+    return render_template('test2.html')
+
+
+if __name__ == '__main__':
+    print(__debug__)
+    app.run(host='0.0.0.0', port=8080, debug=True)
